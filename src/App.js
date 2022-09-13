bb6f5b2d07c68be7158ee9faf59e89647d1d36d0
callmemg <oremgitonga22@gmail.com>
2022-09-13 12:28:42 +0300

    add skills, projects, contact section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import About from "./components/About";
+import Contact from "./components/Contact";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
       <Home />
       <About />
       <Skills />
+      <Projects />
+      <Contact />
     </div>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
       
       {/* container */}
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-        <p className='text-blue-600'>Hi, My name is</p>
+        <p className='text-blue-600 text-2xl'>Hi, My name is</p>
         <h1 className='text-4xl sm:text-6xl font-bold text-[#ccd6f6]'>Orem Gitonga</h1>
         <h2 className='text-4xl sm:text-6xl font-bold text-[#8892b0]'>I'm a Full Stack Developer.</h2>
         <p className='text-[#8892b0] py-4 max-w-[700px]'>
@@ -17,7 +17,7 @@ const Home = () => {
             Blockchain. Before I forget, I am a long-life learner.
         </p>
         <div>
-            <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-orange-500 hover:border-orange-500'>View Projects 
+            <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-blue-500 hover:border-blue-500'>View Projects 
                 <span className='group-hover:rotate-90 duration-300'>
                     <HiArrowNarrowRight className='ml-3' />
                 </span>
